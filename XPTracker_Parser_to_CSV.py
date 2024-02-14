1ff64294ba5560255d2a93c4975d56e3cd81b4f8
Kevin <kcd33@uakron.edu>
2024-02-14 12:57:40 -0500

    Add parsing multiple dungeons and outputting to csv

--- XPTracker_Parser_to_CSV.py
+++ XPTracker_Parser_to_CSV.py
@@ -14,45 +14,94 @@ def findNextVariable(searchData, start, delimeterOne, delimiterTwo):
 
     # if -- does not appear anymore in the file we've reached the end
     if endOfSet == -1:
-        return -1, -1
+        return -1, -1, -1
+    foundValue = searchData[varStart+2:varEnd]
 
-    return varStart, varEnd
+    if(len(foundValue) > 1):
+        if foundValue[1] == '\"':
+            foundValue = foundValue[1:len(foundValue) - 3]
+
+    return varStart, varEnd, foundValue
 
 # goes through entire saved variables file adding categories to the list
 def getCategories(data, begginingDelimeter, endingDelimeter):
     # preload the varStart and varEnd variables
-    varStart, varEnd = findNextVariable(data, 0, begginingDelimeter, endingDelimeter)
+    varStart, varEnd, foundData = findNextVariable(data, 0, begginingDelimeter, endingDelimeter)
     csvVariables = list()
 
-    while (varStart > 0):
-        foundValue = data[varStart+2:varEnd-1]
-
+    while (varStart >= 0):
+        foundValue = data[varStart + 2:varEnd - 1]
+        #foundValue = foundData
         # add value to list if it hasn't been found before
         if foundValue not in csvVariables:
             # categories must be strings, first char past [ will be "
             if data[varStart+1] == '\"':
                 csvVariables.append(foundValue)
 
-        varStart, varEnd = findNextVariable(data, varEnd + 1, begginingDelimeter, endingDelimeter)
+        varStart, varEnd, foundData = findNextVariable(data, varEnd + 1, begginingDelimeter, endingDelimeter)
     #print("the variables are: \n", csvVariables)
     return csvVariables
 
+# returns the category of the data or -2 if no delimeter found
+def getCategory(data, start, begginingDelimeter, endingDelimeter):
+    varStart, varEnd, foundValue = findNextVariable(data, start, begginingDelimeter, endingDelimeter)
+
+    # prevents referencing out of range
+    if(varStart >= 0):
+
+        # if at end of set of data move to next set
+        if data[varStart+1] != '\"':
+            foundValue = -1
+            return varStart, varEnd, foundValue
+
+        foundValue = data[varStart + 2:varEnd - 1]
+        return varStart, varEnd, foundValue
+
+    return -2, -2, -2
 
 
 def getData(data, begginingDelimeter, endingDelimeter, categories):
+    csvVariables = {}
+    compiledData = {}
+    for column in categories:
+        csvVariables[column] = ["No Data"]
+
     # preload the varStart and varEnd variables
-    varStart, varEnd = findNextVariable(data, 0, begginingDelimeter, endingDelimeter)
-    csvVariables = list()
-    #print("varStart is: ", varStart, "varEnd is: ", varEnd)
-    counter = 0
-    while (varStart >= 0) and counter < 20:
-        foundValue = data[varStart+2:varEnd]
+    varStart, varEnd, foundCategory = getCategory(data, 0, begginingDelimeter[0], endingDelimeter[0])
+    varStart, varEnd, foundData = findNextVariable(data, varEnd + 1, begginingDelimeter[1], endingDelimeter[1])
 
-        csvVariables.append(foundValue)
-        counter += 1
-        varStart, varEnd = findNextVariable(data, varEnd + 1, begginingDelimeter, endingDelimeter)
+    while (varStart >= 0):
+        #foundData = data[varStart+2:varEnd]
+        if foundData[0] == '\"':
+            print("found one!: ", foundData)
+            foundData = foundData[1:len(foundData) - 2]
+        csvVariables[foundCategory] = foundData
 
-    return csvVariables
+        # if wowui returns "a" instead of value change to "No Data"
+        if (foundData == 'a'):
+            csvVariables[foundCategory] = ["No Data"]
+
+        varStart, varEnd, foundCategory = getCategory(data, varEnd + 1, begginingDelimeter[0], endingDelimeter[0])
+
+        # add all of the new found dungeon data to the dictionary
+        if foundCategory == -1:
+            if len(compiledData) == 0:
+                for column in categories:
+                    compiledData[column] = [csvVariables[column]]
+                    csvVariables[column] = ["No Data"]
+
+            else:
+                for column in categories:
+                    compiledData[column] += [csvVariables[column]]
+                    csvVariables[column] = ["No Data"]
+
+            varStart, varEnd, foundCategory = getCategory(data, varEnd + 1, begginingDelimeter[0], endingDelimeter[0])
+
+        varStart, varEnd, foundData = findNextVariable(data, varEnd + 1, begginingDelimeter[1], endingDelimeter[1])
+
+    df = pd.DataFrame(compiledData)
+    print("The dataframe is: \n", df)
+    return df
 
 # work in progress
 # creates a DataFrame from the categorie headings filling each category with every value found
@@ -67,21 +116,21 @@ def createDataFrame(columns, rows):
    # print("the combinded data is: \n", combineData)
     df = pd.DataFrame(combineData)
     print("The dataframe is: \n", df)
-    return 0
+    return df
 
 def main():
     dungeonData = Path('./dungeon_runs/XPTracker.lua').read_text()
-    parsedDungeonData = dungeonData[dungeonData.find("\"dungeons\"") + 12:]
-    begginingDelimeter = '['
-    endingDelimeter = ']'
+    parsedDungeonData = dungeonData[dungeonData.find("\"dungeons\"") + 17:]
+    begginingDelimeter = ['[', '=']
+    endingDelimeter = [']', ',']
 
-    catHeadings = getCategories(parsedDungeonData, begginingDelimeter, endingDelimeter)
+    catHeadings = getCategories(parsedDungeonData, begginingDelimeter[0], endingDelimeter[0])
 
-    begginingDelimeter = '='
-    endingDelimeter = ','
-    lvlingData = getData(parsedDungeonData[5:len(parsedDungeonData)-1], begginingDelimeter, endingDelimeter, catHeadings)
+    lvlingData = getData(parsedDungeonData, begginingDelimeter, endingDelimeter, catHeadings)
+    #print("The found data is: ", lvlingData)
+    #createDataFrame(catHeadings, lvlingData)
 
-    createDataFrame(catHeadings, lvlingData)
+    lvlingData.to_csv("./dungeon_runs/dungeonData.csv")
 
 if __name__ == '__main__':
     main()
