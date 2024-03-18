53c6ffc7699727dc01d21da824e7fff65e33507e
Kevin <kcd33@uakron.edu>
2024-03-18 19:14:28 -0400

    Add multiple account handling in path file

--- XPTracker_Parser_to_CSV.py
+++ XPTracker_Parser_to_CSV.py
@@ -126,21 +126,88 @@ def formatDate(rawDate):
 
     return formattedDate
 
+# gets the account name and path to account's saved variables
+def getPaths(pathsDoc):
+    accounts = []
+    savedVarsPath = ""
+
+    # extract the path to account and saved variables from path.txt
+    if pathsDoc.find('WTF\\Account') != -1:
+        savedVarsPath = pathsDoc[0 : pathsDoc.find("WTF\\Account")] + "WTF\\Account\\"
+    else:
+        print("No path found")
+
+    # check if more than one account and extract them from path.txt
+    if pathsDoc.find(',') != -1:
+
+        accountBegin = pathsDoc.find(',') + 2
+        accountEnd = pathsDoc.find(',', accountBegin)
+        while accountEnd != -1:
+            accountPathName = ""
+            accountDisplayName = ""
+
+            accountPathName = pathsDoc[accountBegin : accountEnd]
+            print("accountPathName: ", accountPathName)
+
+            # check if display name for account
+            if len(pathsDoc) >= (accountEnd + 1):
+                if pathsDoc[accountEnd + 1] != '\n':
+                    accountBegin = accountEnd + 2
+                    accountEnd = pathsDoc.find(',', accountBegin)
+
+                    # handles no , after last term
+                    if accountEnd == -1:
+                        if pathsDoc.find('\n', accountBegin) != -1:
+                            accountEnd = pathsDoc.find('\n', accountBegin)
+                        else:
+                            accountEnd = len(pathsDoc) - 1
+                    accountDisplayName = pathsDoc[accountBegin : accountEnd]
+                    print("found display name: ", accountDisplayName)
+
+            accounts.append([accountPathName,accountDisplayName])
+
+            accountBegin = accountEnd + 2
+            accountEnd = pathsDoc.find(',', accountBegin)
+
+        # handles no ',' after last account
+        if len(pathsDoc) > accountBegin + 3:
+            if pathsDoc.find('\n', accountBegin) != -1:
+                accountPathName = pathsDoc[accountBegin : pathsDoc.find('\n', accountBegin)]
+            else:
+                accountPathName = pathsDoc[accountBegin : ]
+            print("accountPathName: ", accountPathName)
+            accounts.append([accountPathName, ""])
+
+    else:
+        print("No account found")
+
+    return accounts, savedVarsPath
+
 def main():
     path = Path('./path.txt').read_text()
-    dungeonData = Path(path).read_text()
-    parsedDungeonData = dungeonData[dungeonData.find("\"dungeons\"") + 17:]
-    begginingDelimeter = ['[', '=']
-    endingDelimeter = [']', ',']
+    accounts, savedVarsPath = getPaths(path)
+    accountCount = len(accounts)
 
-    # get key values
-    catHeadings = getCategories(parsedDungeonData, begginingDelimeter[0], endingDelimeter[0])
+    for i in range(0, accountCount):
+        accountPath = savedVarsPath + accounts[i][0] + "\\SavedVariables\\DungeonXPTracker.lua"
+        dungeonData = Path(accountPath).read_text()
+        parsedDungeonData = dungeonData[dungeonData.find("\"dungeons\"") + 17:]
+        begginingDelimeter = ['[', '=']
+        endingDelimeter = [']', ',']
 
-    # use key values to map data to keys and get DataFrame from them
-    lvlingData = getData(parsedDungeonData, begginingDelimeter, endingDelimeter, catHeadings)
+        # get key values
+        catHeadings = getCategories(parsedDungeonData, begginingDelimeter[0], endingDelimeter[0])
 
-    # output DataFrame as csv
-    lvlingData.to_csv("./dungeon_runs/dungeonData.csv")
+        # use key values to map data to keys and get DataFrame from them
+        lvlingData = getData(parsedDungeonData, begginingDelimeter, endingDelimeter, catHeadings)
+
+        # output DataFrame as csv
+        accountDisplayName = 0
+        if accounts[i][1] != "":
+            accountDisplayName = 1
+
+        exportPath = "./dungeon_runs/" + accounts[i][accountDisplayName] + "dungeonData.csv"
+        lvlingData.to_csv(exportPath)
 
     print("Parsing Complete")
 
