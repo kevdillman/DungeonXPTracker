9850cf826a7dd45e58ded57bee0a33253b74e8d5
Keri Sue Dillman <kersuedillman@gmail.com>
2024-04-28 16:30:06 -0400

    Add person and account table models

--- SQLIntegration.py
+++ SQLIntegration.py
@@ -10,16 +10,18 @@ password = "pythonClientPasswordStrong!!1133$$"
 hostNamePort = "127.0.0.1,1337"
 dbName = "dungeonxptrackerdb"
 
+# returns current drivers for pyodbc on system
 #driverNames = [x for x in pyodbc.drivers() if x.endswith(' for SQL Server')]
 #print(pyodbc.drivers())
 
+# non-working code to create SQLAlchemy engine
 #engine = create_engine("mssql+pyodbc://" + userName +":" + password + hostNamePort + "/" + dbName + "?driver=ODBC+Driver+17+for+SQL+Server")
 
+# creates connection to pyodbc database 
 connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};ADDRESS={hostNamePort};UID={userName};PWD={password};DATABASE={dbName};TrustServerCertificate=YES'
 conn = pyodbc.connect(connectionString)
 
-
-
+# storing sql query
 SQL_QUERY = """
 SELECT *
 FROM INFORMATION_SCHEMA.COLUMNS
--- models.py
+++ models.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+from optparse import Option
+from turtle import back
 from typing import List
 from typing import Optional
-from sqlalchemy import ForeignKey
+from pandas import Timestamp
+from sqlalchemy import ForeignKey, true
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
@@ -16,3 +20,27 @@ class Person(Base):
     __tablename__ = "person"
 
     pID: Mapped[int] = mapped_column(primary_key=True)
+    pextID: Mapped[Optional[str]]
+    firstNAME: Mapped[str]
+    middleNAME: Mapped[Optional[str]]
+    lastNAME: Mapped[str]
+    email: Mapped[str]
+    pcreationDT: Mapped[Timestamp]
+
+    accounts: Mapped[List["Account"]] = relationship(back_populates="person")
+
+    def __repr__(self) -> str:
+        return f"Person(pID={self.pID!r}, pextID={self.pextID!r}, firstNAME={self.firstNAME!r}, middleNAME={self.firstNAME!r}, lastNAME{self.lastNAME!r}, email={self.email!r}, pcreationDT={self.pcreationDT!r}"
+    
+class Account(Base):
+    __tablename__ = "account"
+
+    aID: Mapped[int] = mapped_column(primary_key=true)
+    aextID: Mapped[Optional[str]]
+    bnetNAME: Mapped[str]
+    personID: Mapped[int] = mapped_column(ForeignKey("person.pID"))
+
+    person: Mapped[List["Person"]] = relationship(back_populates="accounts")
+
+    def __repr__(self) -> str:
+        return f"Account(aID={self.aID!r}, aextID={self.aextID!r}, bnetNAME={self.bnetNAME!r}, personID={self.personID!r})"
