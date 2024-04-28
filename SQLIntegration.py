5328dee5d45684ce7b5aceaff5037d2767fbd4d8
Kevin <kcd33@uakron.edu>
2024-04-28 13:54:54 -0400

    Add initla SQL Alchemy files

---
+++ SQLIntegration.py
@@ -0,0 +1,33 @@
+# Class to implement Azure SQL Database integration
+
+from sqlalchemy.engine import URL
+import pyodbc
+import models
+
+# load the database connection into the engine
+userName = "pyClient"
+password = "pythonClientPasswordStrong!!1133$$"
+hostNamePort = "127.0.0.1,1337"
+dbName = "dungeonxptrackerdb"
+
+#driverNames = [x for x in pyodbc.drivers() if x.endswith(' for SQL Server')]
+#print(pyodbc.drivers())
+
+#engine = create_engine("mssql+pyodbc://" + userName +":" + password + hostNamePort + "/" + dbName + "?driver=ODBC+Driver+17+for+SQL+Server")
+
+connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};ADDRESS={hostNamePort};UID={userName};PWD={password};DATABASE={dbName};TrustServerCertificate=YES'
+conn = pyodbc.connect(connectionString)
+
+
+
+SQL_QUERY = """
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+"""
+
+cursor = conn.cursor()
+cursor.execute(SQL_QUERY)
+
+records = cursor.fetchall()
+#for r in records:
+    #print(r)
---
+++ models.py
@@ -0,0 +1,10 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+print("models imported.")
