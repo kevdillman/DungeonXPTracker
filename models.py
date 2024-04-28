d89819718cd074bef4d46aeda05e92852decaabb
Keri Sue Dillman <kersuedillman@gmail.com>
2024-04-28 17:43:57 -0400

    Add Character model

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+import datetime
 from optparse import Option
 from turtle import back
 from typing import List
 from typing import Optional
 from pandas import Timestamp
-from sqlalchemy import ForeignKey, true
+from sqlalchemy import ForeignKey, BIGINT, TIMESTAMP, NVARCHAR, func
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
@@ -14,7 +14,11 @@ from sqlalchemy.orm import relationship
 print("models imported.")
 
 class Base(DeclarativeBase):
-    pass
+    type_annotation_map = {
+        int: BIGINT,
+        datetime.datetime: TIMESTAMP(timezone=True),
+        str: String().with_variant(NVARCHAR, "mssql"),
+    }
 
 class Person(Base):
     __tablename__ = "person"
@@ -25,7 +29,7 @@ class Person(Base):
     middleNAME: Mapped[Optional[str]]
     lastNAME: Mapped[str]
     email: Mapped[str]
-    pcreationDT: Mapped[Timestamp]
+    pcreationDT: Mapped[datetime.datetime] = mapped_column(server_default = func.CURRENT_TIMESTAMP())
 
     accounts: Mapped[List["Account"]] = relationship(back_populates="person")
 
@@ -35,12 +39,33 @@ class Person(Base):
 class Account(Base):
     __tablename__ = "account"
 
-    aID: Mapped[int] = mapped_column(primary_key=true)
+    aID: Mapped[int] = mapped_column(primary_key=True)
     aextID: Mapped[Optional[str]]
     bnetNAME: Mapped[str]
     personID: Mapped[int] = mapped_column(ForeignKey("person.pID"))
 
-    person: Mapped[List["Person"]] = relationship(back_populates="accounts")
+    person: Mapped["Person"] = relationship(back_populates="accounts")
+    characters: Mapped[List["Character"]] = relationship(back_populates="account")
 
     def __repr__(self) -> str:
         return f"Account(aID={self.aID!r}, aextID={self.aextID!r}, bnetNAME={self.bnetNAME!r}, personID={self.personID!r})"
+
+class Character(Base):
+    __tablename__ = "character"
+
+    cID: Mapped[int] = mapped_column(primary_key=True)
+    cRV: Mapped[datetime.datetime] = mapped_column(server_default = func.CURRENT_TIMESTAMP())
+    cextID: Mapped[Optional[str]]
+    cNAME: Mapped[str]
+    cREALM: Mapped[str]
+    cFACTION: Mapped[str]
+    cRACE: Mapped[str]
+    cGUILD: Mapped[Optional[str]]
+    accountID: Mapped[int] = mapped_column(ForeignKey("account.aID"))
+
+    account: Mapped["Account"] = relationship(back_populates="characters")
+    #dungeons: Mapped["DungeonRun"] = relationship(back_populates="character")
+
+    def __repr__(self) -> str: 
+        return f"Character(cNAME={self.cNAME!r}, cREALM={self.cREALM!r}, cFACTION={self.cFACTION!r}, cRACE={self.cRACE!r}, cGUILD={self.cGUILD!r}, accountID={self.accountID}, cID={self.cID!r})"
+    
